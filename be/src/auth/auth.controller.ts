f5e41af4f6ebe04634d678aec9a187dd3f442698
datanh98hp <dat198hp@gmail.com>
2023-12-22 13:48:19 +0700

    update

--- be/src/auth/auth.controller.ts
+++ be/src/auth/auth.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from 'src/dto/Auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService,
+    ) { }
+
+    @Get()
+    test() {
+        return 'asdada';
+    }
+    @Post('login')
+    async login(@Body() userLogin: AuthDto) {
+        return await this.authService.signIn(userLogin);
+        // return "login";
+    }
+    @Post('register')
+    async register(@Body() userRegister: AuthDto) {
+        return await this.authService.register(userRegister);
+    }
+    @Post('refresh-token')
+    async refreshToken(@Body() token: string) {
+        return await this.authService.refreshToken(token);
+    }
+}
